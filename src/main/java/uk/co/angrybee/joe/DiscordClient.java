24afba37a7d129196f3ed5dc8e1262d643a4680b
Joe Shimell <49585339+JoeShimell@users.noreply.github.com>
2021-03-03 14:09:40 +0000

    Only filter out self, not other bots/webhooks

--- src/main/java/uk/co/angrybee/joe/DiscordClient.java
+++ src/main/java/uk/co/angrybee/joe/DiscordClient.java
@@ -320,7 +320,7 @@ public class DiscordClient extends ListenerAdapter
             if (!Arrays.asList(targetTextChannels).contains(messageReceivedEvent.getTextChannel().getId()))
                 return;
 
-            if (messageReceivedEvent.getAuthor().isBot())
+            if(messageReceivedEvent.getAuthor().getIdLong() == javaDiscordAPI.getSelfUser().getIdLong())
                 return;
 
             String messageContents = messageReceivedEvent.getMessage().getContentRaw();
